7bef653cb10509b4d24e5e8132d7178b288a34a9
zZHorizonZz <danfiala23@gmail.com>
2022-11-28 13:02:02 +0100

    Animation tweek of Switch component.

--- Material.Components.Maui/Components/Switch/Switch.cs
+++ Material.Components.Maui/Components/Switch/Switch.cs
@@ -6,11 +6,16 @@ using System.Runtime.CompilerServices;
 using System.Windows.Input;
 
 namespace Material.Components.Maui;
-public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeElement, IStateLayerElement, IRippleElement
+
+public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeElement, IStateLayerElement,
+    IRippleElement
 {
     #region interface
+
     #region IView
+
     private ControlState controlState = ControlState.Normal;
+
     public ControlState ControlState
     {
         get => this.controlState;
@@ -20,6 +25,7 @@ public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeE
             this.ChangeVisualState();
         }
     }
+
     protected override void ChangeVisualState()
     {
         var state = this.ControlState switch
@@ -32,77 +38,98 @@ public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeE
         };
         VisualStateManager.GoToState(this, state);
     }
+
     public void OnPropertyChanged()
     {
         this.InvalidateSurface();
     }
+
     #endregion
 
     #region IOutlineElement
+
     public static readonly BindableProperty OutlineColorProperty = OutlineElement.OutlineColorProperty;
     public static readonly BindableProperty OutlineWidthProperty = OutlineElement.OutlineWidthProperty;
     public static readonly BindableProperty OutlineOpacityProperty = OutlineElement.OutlineOpacityProperty;
+
     public Color OutlineColor
     {
         get => (Color)this.GetValue(OutlineColorProperty);
         set => this.SetValue(OutlineColorProperty, value);
     }
+
     public int OutlineWidth
     {
         get => (int)this.GetValue(OutlineWidthProperty);
         set => this.SetValue(OutlineWidthProperty, value);
     }
+
     public float OutlineOpacity
     {
         get => (float)this.GetValue(OutlineOpacityProperty);
         set => this.SetValue(OutlineOpacityProperty, value);
     }
+
     #endregion
 
     #region IShapeElement
+
     public static readonly BindableProperty ShapeProperty = ShapeElement.ShapeProperty;
+
     public Shape Shape
     {
         get => (Shape)this.GetValue(ShapeProperty);
         set => this.SetValue(ShapeProperty, value);
     }
+
     #endregion
 
     #region IStateLayerElement
+
     public static readonly BindableProperty StateLayerColorProperty = StateLayerElement.StateLayerColorProperty;
     public static readonly BindableProperty StateLayerOpacityProperty = StateLayerElement.StateLayerOpacityProperty;
+
     public Color StateLayerColor
     {
         get => (Color)this.GetValue(StateLayerColorProperty);
         set => this.SetValue(StateLayerColorProperty, value);
     }
+
     public float StateLayerOpacity
     {
         get => (float)this.GetValue(StateLayerOpacityProperty);
         set => this.SetValue(StateLayerOpacityProperty, value);
     }
+
     #endregion
 
     #region IRippleElement
+
     public static readonly BindableProperty RippleColorProperty = RippleElement.RippleColorProperty;
+
     public Color RippleColor
     {
         get => (Color)this.GetValue(RippleColorProperty);
         set => this.SetValue(RippleColorProperty, value);
     }
+
     [EditorBrowsable(EditorBrowsableState.Never)]
     public float RippleSize { get; private set; } = 0f;
+
     [EditorBrowsable(EditorBrowsableState.Never)]
     public float RipplePercent { get; set; } = 0f;
+
     [EditorBrowsable(EditorBrowsableState.Never)]
     public SKPoint TouchPoint { get; set; } = new SKPoint(-1, -1);
+
     #endregion
+
     #endregion
 
     [AutoBindable(OnChanged = nameof(OnCheckedChanged))]
     private readonly bool isChecked;
 
-    [AutoBindable]
+    [AutoBindable] 
     private readonly Color trackColor;
 
     [AutoBindable(DefaultValue = "1f")]
@@ -113,6 +140,9 @@ public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeE
 
     [AutoBindable(DefaultValue = "1f")]
     private readonly float thumbOpacity;
+
+    [AutoBindable(DefaultValue = "true")]
+    private readonly bool isIconVisible;
 
     [AutoBindable]
     private readonly Color iconColor;
@@ -128,11 +158,9 @@ public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeE
         this.Command?.Execute(this.CommandParameter ?? this.IsChecked);
     }
 
-    [AutoBindable]
-    private readonly ICommand command;
+    [AutoBindable] private readonly ICommand command;
 
-    [AutoBindable]
-    private readonly object commandParameter;
+    [AutoBindable] private readonly object commandParameter;
 
     public event EventHandler<CheckedChangedEventArgs> CheckedChanged;
 
@@ -155,12 +183,12 @@ public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeE
         var start = 0f;
         var end = 1f;
         this.animationManager?.Add(new Microsoft.Maui.Animations.Animation(callback: (progress) =>
-        {
-            this.ChangingPercent = start.Lerp(end, progress);
-            this.InvalidateSurface();
-        },
-        duration: 0.25f,
-        easing: Easing.SinInOut));
+            {
+                this.ChangingPercent = start.Lerp(end, progress);
+                this.InvalidateSurface();
+            },
+            duration: 0.75f,
+            easing: Easing.SinInOut));
     }
 
 
@@ -173,25 +201,26 @@ public partial class Switch : SKTouchCanvasView, IView, IOutlineElement, IShapeE
         var end = 1f;
 
         this.animationManager?.Add(new Microsoft.Maui.Animations.Animation(callback: (progress) =>
-        {
-            this.RipplePercent = start.Lerp(end, progress);
-            this.InvalidateSurface();
-        },
-        duration: 0.35f,
-        easing: Easing.SinInOut,
-        finished: () =>
-        {
-            if (this.ControlState != ControlState.Pressed)
             {
-                this.RipplePercent = 0;
+                this.RipplePercent = start.Lerp(end, progress);
                 this.InvalidateSurface();
-            }
-        }));
+            },
+            duration: 0.35f,
+            easing: Easing.SinInOut,
+            finished: () =>
+            {
+                if (this.ControlState != ControlState.Pressed)
+                {
+                    this.RipplePercent = 0;
+                    this.InvalidateSurface();
+                }
+            }));
     }
 
     protected override void OnPaintSurface(SKPaintSurfaceEventArgs e)
     {
-        var bounds = new SKRect(e.Info.Rect.Left + 8, e.Info.Rect.Top + 8, e.Info.Rect.Right - 8, e.Info.Rect.Bottom - 8);
+        var bounds = new SKRect(e.Info.Rect.Left + 8, e.Info.Rect.Top + 8, e.Info.Rect.Right - 8,
+            e.Info.Rect.Bottom - 8);
         this.drawable.Draw(e.Surface.Canvas, bounds);
     }
 
