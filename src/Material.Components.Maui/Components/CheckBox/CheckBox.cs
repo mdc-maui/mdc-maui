60a7374edecd25b5706d7cc59cc3ea27de10afab
yiszza <42995476+yiszza@users.noreply.github.com>
2023-06-05 19:57:42 +0800

    Migrating CheckBox to Microsoft.Maui.Graphics

---
+++ src/Material.Components.Maui/Components/CheckBox/CheckBox.cs
@@ -0,0 +1,85 @@
+﻿namespace Material.Components.Maui;
+
+public class CheckBox : TouchGraphicView, IOutlineElement, IIconElement
+{
+    protected override void ChangeVisualState()
+    {
+        this.IsVisualStateChanging = true;
+        var state = this.ViewState switch
+        {
+            ViewState.Normal => "normal",
+            ViewState.Hovered => "hovered",
+            ViewState.Pressed => "pressed",
+            ViewState.Disabled => "disabled",
+            _ => "normal",
+        };
+        VisualStateManager.GoToState(this, state);
+        this.IsVisualStateChanging = false;
+
+        if (!this.IsFocused)
+            this.Invalidate();
+    }
+
+    public static readonly BindableProperty IconDataProperty = IIconElement.IconDataProperty;
+    public static readonly BindableProperty IconColorProperty = IIconElement.IconColorProperty;
+    public static readonly BindableProperty OutlineWidthProperty =
+        IOutlineElement.OutlineWidthProperty;
+    public static readonly BindableProperty OutlineColorProperty =
+        IOutlineElement.OutlineColorProperty;
+    public static BindableProperty IsCheckedProperty = BindableProperty.Create(
+        nameof(IsChecked),
+        typeof(bool),
+        typeof(CheckBox),
+        false,
+        propertyChanged: (bo, ov, nv) =>
+        {
+            var cb = (CheckBox)bo;
+            cb.CheckedChanged?.Invoke(cb, new CheckedChangedEventArgs(cb.IsChecked));
+            cb.Command?.Execute(cb.CommandParameter ?? cb.IsChecked);
+        }
+    );
+
+    public Color OutlineColor
+    {
+        get => (Color)this.GetValue(OutlineColorProperty);
+        set => this.SetValue(OutlineColorProperty, value);
+    }
+
+    public int OutlineWidth
+    {
+        get => (int)this.GetValue(OutlineWidthProperty);
+        set => this.SetValue(OutlineWidthProperty, value);
+    }
+
+    public string IconData
+    {
+        get => (string)this.GetValue(IconDataProperty);
+        set => this.SetValue(IconDataProperty, value);
+    }
+    PathF IIconElement.IconPath { get; set; }
+
+    public Color IconColor
+    {
+        get => (Color)this.GetValue(IconColorProperty);
+        set => this.SetValue(IconColorProperty, value);
+    }
+
+    public bool IsChecked
+    {
+        get => (bool)this.GetValue(IsCheckedProperty);
+        set => this.SetValue(IsCheckedProperty, value);
+    }
+
+    event EventHandler<CheckedChangedEventArgs> CheckedChanged;
+
+    public CheckBox()
+    {
+        this.Drawable = new CheckBoxDrawable(this);
+        this.EndInteraction += this.OnCheckChanged;
+    }
+
+    private void OnCheckChanged(object sender, TouchEventArgs e)
+    {
+        this.IsChecked = !this.IsChecked;
+    }
+}
---
+++ src/Material.Components.Maui/Components/CheckBox/CheckBoxDrawable.cs
@@ -0,0 +1,55 @@
+﻿using Material.Components.Maui.Extensions;
+
+namespace Material.Components.Maui;
+
+internal class CheckBoxDrawable : IDrawable
+{
+    readonly CheckBox view;
+
+    public CheckBoxDrawable(CheckBox view)
+    {
+        this.view = view;
+    }
+
+    public void Draw(ICanvas canvas, RectF rect)
+    {
+        canvas.Antialias = true;
+        var scale = rect.Height / 40f;
+        var drawRect = new RectF(11 * scale, 11 * scale, 18 * scale, 18 * scale);
+        if (this.view.IsChecked)
+        {
+            canvas.SaveState();
+            canvas.ClipPath(this.view.GetClipPath(drawRect));
+            canvas.DrawBackground(this.view, drawRect);
+            canvas.RestoreState();
+        }
+
+        this.DrawStateLayer(canvas, rect);
+
+        if (this.view.IsChecked)
+            canvas.DrawIcon(this.view, drawRect, 18, scale);
+        else
+            canvas.DrawOutline(this.view, drawRect);
+    }
+
+    void DrawStateLayer(ICanvas canvas, RectF rect)
+    {
+        canvas.SaveState();
+        var drawRect = new PathF();
+        drawRect.AppendCircle(rect.Center.X, rect.Center.Y, Math.Max(rect.Width, rect.Height) / 2f);
+        canvas.ClipPath(drawRect);
+
+        if (this.view.RipplePercent == 0f)
+            canvas.DrawStateLayer(this.view, rect, this.view.ViewState);
+
+        if (this.view.RipplePercent != 0f)
+            canvas.DrawRipple(
+                this.view,
+                this.view.LastTouchPoint,
+                this.view.RippleSize,
+                this.view.RipplePercent
+            );
+
+        canvas.RestoreState();
+    }
+}
