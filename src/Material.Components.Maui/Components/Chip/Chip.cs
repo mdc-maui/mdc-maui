fe317d799a46d5fa94461cf33f37b94db00eecbb
yiszza <42995476+yiszza@users.noreply.github.com>
2023-06-08 03:06:18 +0800

    Migrating Chip to Microsoft.Maui.Graphics

---
+++ src/Material.Components.Maui/Components/Chip/Chip.cs
@@ -0,0 +1,220 @@
+﻿using Material.Components.Maui.Extensions;
+using Material.Components.Maui.Styles;
+using System.ComponentModel;
+
+namespace Material.Components.Maui;
+
+public class Chip : TouchGraphicView, IIconElement, ITextElement, IOutlineElement, IElevationElement
+{
+    protected override void ChangeVisualState()
+    {
+        this.IsVisualStateChanging = true;
+        var state = this.ViewState switch
+        {
+            ViewState.Normal => this.IsSelected ? "selected_normal" : "unselected_normal",
+            ViewState.Hovered => this.IsSelected ? "selected_hovered" : "unselected_hovered",
+            ViewState.Pressed => this.IsSelected ? "selected_pressed" : "unselected_pressed",
+            ViewState.Disabled => this.IsSelected ? "selected_disabled" : "unselected_disabled",
+            _ => "unselected_normal",
+        };
+        VisualStateManager.GoToState(this, state);
+        this.IsVisualStateChanging = false;
+
+        this.Invalidate();
+    }
+
+    public static readonly BindableProperty IsSelectedProperty = BindableProperty.Create(
+        nameof(IsSelected),
+        typeof(bool),
+        typeof(Chip),
+        false,
+        propertyChanged: (bo, ov, nv) => ((Chip)bo).ChangeVisualState()
+    );
+    public static readonly BindableProperty HasCloseButtonProperty = BindableProperty.Create(
+        nameof(HasCloseButton),
+        typeof(bool),
+        typeof(Chip),
+        false,
+        propertyChanged: (bo, ov, nv) => ((IElement)bo).InvalidateMeasure()
+    );
+
+    public static readonly BindableProperty TextProperty = ITextElement.TextProperty;
+    public static readonly BindableProperty TextColorProperty = ITextElement.TextColorProperty;
+    public static readonly BindableProperty FontSizeProperty = ITextElement.FontSizeProperty;
+    public static readonly BindableProperty FontFamilyProperty = ITextElement.FontFamilyProperty;
+    public static readonly BindableProperty FontSlantProperty = ITextElement.FontSlantProperty;
+    public static readonly BindableProperty FontWeightProperty = ITextElement.FontWeightProperty;
+
+    public static readonly BindableProperty IconDataProperty = IIconElement.IconDataProperty;
+    public static readonly BindableProperty IconColorProperty = IIconElement.IconColorProperty;
+
+    public static readonly BindableProperty OutlineWidthProperty =
+        IOutlineElement.OutlineWidthProperty;
+    public static readonly BindableProperty OutlineColorProperty =
+        IOutlineElement.OutlineColorProperty;
+    public static readonly BindableProperty ElevationProperty = IElevationElement.ElevationProperty;
+
+    public bool IsSelected
+    {
+        get => (bool)this.GetValue(IsSelectedProperty);
+        set => this.SetValue(IsSelectedProperty, value);
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public bool HasCloseButton
+    {
+        get => (bool)this.GetValue(HasCloseButtonProperty);
+        set => this.SetValue(HasCloseButtonProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)this.GetValue(TextProperty);
+        set => this.SetValue(TextProperty, value);
+    }
+    public Color TextColor
+    {
+        get => (Color)this.GetValue(TextColorProperty);
+        set => this.SetValue(TextColorProperty, value);
+    }
+    public float FontSize
+    {
+        get => (float)this.GetValue(FontSizeProperty);
+        set => this.SetValue(FontSizeProperty, value);
+    }
+    public string FontFamily
+    {
+        get => (string)this.GetValue(FontFamilyProperty);
+        set => this.SetValue(FontFamilyProperty, value);
+    }
+    public FontSlant FontSlant
+    {
+        get => (FontSlant)this.GetValue(FontSlantProperty);
+        set => this.SetValue(FontSlantProperty, value);
+    }
+    public FontWeight FontWeight
+    {
+        get => (FontWeight)this.GetValue(FontWeightProperty);
+        set => this.SetValue(FontWeightProperty, value);
+    }
+
+    public string IconData
+    {
+        get => (string)this.GetValue(IconDataProperty);
+        set => this.SetValue(IconDataProperty, value);
+    }
+
+    PathF IIconElement.IconPath { get; set; }
+
+    public Color IconColor
+    {
+        get => (Color)this.GetValue(IconColorProperty);
+        set => this.SetValue(IconColorProperty, value);
+    }
+    public Color OutlineColor
+    {
+        get => (Color)this.GetValue(OutlineColorProperty);
+        set => this.SetValue(OutlineColorProperty, value);
+    }
+
+    public int OutlineWidth
+    {
+        get => (int)this.GetValue(OutlineWidthProperty);
+        set => this.SetValue(OutlineWidthProperty, value);
+    }
+    public Elevation Elevation
+    {
+        get => (Elevation)this.GetValue(ElevationProperty);
+        set => this.SetValue(ElevationProperty, value);
+    }
+
+    public event EventHandler OnClosed;
+
+    protected IFontManager fontManager;
+
+    static Style defaultStyle;
+
+    public Chip()
+    {
+        this.Style = defaultStyle ??= ResourceExtension.MaterialDictionaries
+            .First(x => x.GetType() == typeof(ChipStyles))
+            .FindStyle("FilterChipStyle");
+
+        this.Drawable = new ChipDrawable(this);
+        this.EndInteraction += this.OnPressed;
+    }
+
+    protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
+    {
+        var maxWidth = Math.Min(
+            Math.Min(widthConstraint, this.MaximumWidthRequest),
+            this.WidthRequest != -1 ? this.WidthRequest : double.PositiveInfinity
+        );
+        var maxHeight = Math.Min(
+            Math.Min(heightConstraint, this.MaximumHeightRequest),
+            this.HeightRequest != -1 ? this.HeightRequest : double.PositiveInfinity
+        );
+
+        var scale =
+            (this.HeightRequest != -1 ? this.HeightRequest : Math.Min(32f, maxHeight)) / 32f;
+        var iconSize = (!string.IsNullOrEmpty(this.IconData) ? 18f : 0f) * scale;
+        var closeSize = this.HasCloseButton ? 18f * scale : 0f;
+        var textSize = this.GetStringSize();
+        //16 + iconSize + textSize.Width + closeBtnSize +  16
+        var needWidth = 32f * scale + iconSize + textSize.Width + closeSize;
+        //8 + (iconSize || 18)  + 8;
+        var needHeight = 16f * scale + Math.Max(iconSize, 18f * scale);
+
+        var width =
+            this.HorizontalOptions.Alignment == LayoutAlignment.Fill
+                ? maxWidth
+                : this.Margin.HorizontalThickness
+                    + Math.Max(
+                        this.MinimumWidthRequest,
+                        this.WidthRequest == -1 ? Math.Min(maxWidth, needWidth) : this.WidthRequest
+                    );
+        var height =
+            this.VerticalOptions.Alignment == LayoutAlignment.Fill
+                ? maxHeight
+                : this.Margin.VerticalThickness
+                    + Math.Max(
+                        this.MinimumHeightRequest,
+                        this.HeightRequest == -1
+                            ? Math.Min(maxHeight, needHeight)
+                            : this.HeightRequest
+                    );
+
+        this.DesiredSize = new Size(Math.Ceiling(width), Math.Ceiling(height));
+        return this.DesiredSize;
+    }
+
+    private void OnPressed(object sender, TouchEventArgs e)
+    {
+        if (this.HasCloseButton)
+        {
+            var scale = (float)this.Bounds.Height / 32f;
+            var closeRect = new RectF(
+                (float)(this.Bounds.Width - (8f + 16f) * scale),
+                8f * scale,
+                18f * scale,
+                18f * scale
+            );
+            if (closeRect.Contains(e.Touches[0]))
+            {
+                this.OnClosed?.Invoke(this, e);
+                return;
+            }
+        }
+
+        this.IsSelected = !this.IsSelected;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (!this.disposedValue && disposing)
+        {
+            this.EndInteraction -= this.OnPressed;
+        }
+        base.Dispose(disposing);
+    }
+}
---
+++ src/Material.Components.Maui/Components/Chip/ChipDrawable.cs
@@ -0,0 +1,81 @@
+﻿using Material.Components.Maui.Extensions;
+
+namespace Material.Components.Maui;
+
+internal class ChipDrawable : IDrawable
+{
+    readonly Chip view;
+
+    readonly PathF closePath = PathBuilder.Build(
+        "M6.4,19 L5,17.6 10.6,12 5,6.4 6.4,5 12,10.6 17.6,5 19,6.4 13.4,12 19,17.6 17.6,19 12,13.4Z"
+    );
+
+    public ChipDrawable(Chip view)
+    {
+        this.view = view;
+    }
+
+    public void Draw(ICanvas canvas, RectF rect)
+    {
+        canvas.SaveState();
+        canvas.Antialias = true;
+        canvas.ClipPath(this.view.GetClipPath(rect));
+
+        canvas.DrawBackground(this.view, rect);
+        canvas.DrawOutline(this.view, rect);
+        canvas.DrawOverlayLayer(this.view, rect);
+
+        var scale = rect.Height / 32f;
+        var closeRect = new RectF(
+            rect.Width - (8f + 18f) * scale,
+            8f * scale,
+            18f * scale,
+            18f * scale
+        );
+
+        if (this.view.RipplePercent is 0f or 1f)
+            canvas.DrawStateLayer(this.view, rect, this.view.ViewState);
+        else
+            canvas.DrawRipple(
+                this.view,
+                this.view.LastTouchPoint,
+                this.view.RippleSize,
+                this.view.RipplePercent
+            );
+
+        canvas.DrawIcon(
+            this.view,
+            new RectF(8f * scale, 8f * scale, 18f * scale, 18f * scale),
+            18,
+            scale
+        );
+        var iconSize = (!string.IsNullOrEmpty(this.view.IconData) ? 18f : 0f) * scale;
+        canvas.DrawText(
+            this.view,
+            new RectF(
+                iconSize,
+                0f,
+                rect.Width - iconSize - (this.view.HasCloseButton ? 18f * scale : 0f),
+                rect.Height
+            )
+        );
+
+        if (this.view.HasCloseButton)
+            this.DrawCloseIcon(canvas, this.view, closeRect, scale);
+
+        canvas.ResetState();
+    }
+
+    void DrawCloseIcon(ICanvas canvas, IIconElement element, RectF rect, float scale)
+    {
+        canvas.FillColor = element.IconColor.WithAlpha(
+            element.ViewState is ViewState.Disabled ? 0.38f : 1f
+        );
+
+        var path = this.closePath.AsScaledPath(18f / 24f * scale);
+        var sx = rect.Center.X - 18f / 2 * scale;
+        var sy = rect.Center.Y - 18f / 2 * scale;
+        path.Move(sx, sy);
+        canvas.FillPath(path);
+    }
+}
